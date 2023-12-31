3a0677dcba0c0f56f39dc8dc8a9b3420ba16e0ed
jlegacy-ai <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2024-01-01 03:30:12 +0500

    API add

--- src/constants/apiLinks.js
+++ src/constants/apiLinks.js
@@ -11,57 +11,20 @@ export const apis = {
     return axios.get(`${BASE_URL}/products/${id}`);
   },
   getCategories: () => {
-    return axios.get(`${BASE_URL}/categories`);
+    return axios.get(`${BASE_URL}/products/categories`);
   },
-  getCategory: (id) => {
-    return axios.get(`${BASE_URL}/categories/${id}`);
+  getCategory: (category) => {
+    return axios.get(`${BASE_URL}/categories/${category}`);
   },
-  getProductsByCategory: (id) => {
-    return axios.get(`${BASE_URL}/categories/${id}/products`);
+  addNewProduct: (product) => {
+    return axios.post(`${BASE_URL}/products`, {
+      ...product,
+    });
   },
-  getCart: () => {
-    return axios.get(`${BASE_URL}/cart`);
-  },
-  addToCart: (id) => {
-    return axios.post(`${BASE_URL}/cart`, { id });
-  },
-  removeFromCart: (id) => {
-    return axios.delete(`${BASE_URL}/cart/${id}`);
-  },
-  updateCart: (id, quantity) => {
-    return axios.put(`${BASE_URL}/cart/${id}`, { quantity });
-  },
-  getWishlist: () => {
-    return axios.get(`${BASE_URL}/wishlist`);
-  },
-  addToWishlist: (id) => {
-    return axios.post(`${BASE_URL}/wishlist`, { id });
-  },
-  removeFromWishlist: (id) => {
-    return axios.delete(`${BASE_URL}/wishlist/${id}`);
-  },
-  getOrders: () => {
-    return axios.get(`${BASE_URL}/orders`);
-  },
-  createOrder: (data) => {
-    return axios.post(`${BASE_URL}/orders`, data);
-  },
-  getOrder: (id) => {
-    return axios.get(`${BASE_URL}/orders/${id}`);
-  },
-  getOrdersByUser: () => {
-    return axios.get(`${BASE_URL}/users/orders`);
-  },
-  getOrdersByUserAndStatus: (status) => {
-    return axios.get(`${BASE_URL}/users/orders?status=${status}`);
-  },
-  cancelOrder: (id) => {
-    return axios.put(`${BASE_URL}/orders/${id}/cancel`);
-  },
-  payOrder: (id) => {
-    return axios.put(`${BASE_URL}/orders/${id}/pay`);
-  },
-  deliverOrder: (id) => {
-    return axios.put(`${BASE_URL}/orders/${id}/deliver`);
+  updateProduct: (product) => {
+    const { id, ...otherProps } = product;
+    return axios.put(`${BASE_URL}/products/${id}`, {
+      ...otherProps,
+    });
   },
 };
