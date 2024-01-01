665c8f0308f4c0d0a45f1d30cae3ca908dcc5fc2
jlegacy-ai <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2024-01-01 09:17:32 +0500

    Context API

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import { useState } from "react";
+import { CartProducts } from "./hooks/hooks";
 
 function App() {
+  const [cart, setCart] = useState([]);
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
+      <CartProducts.Provider value={{ cart, setCart }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </CartProducts.Provider>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
